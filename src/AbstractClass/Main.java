0a8245ed6f66a946c61d530207bd825a8fd7e248
Bartłomiej Perliński <bartek.perlinski1234@gmail.com>
2022-06-29 10:18:09 +0200

    Added some new tasks

--- src/AbstractClass/Main.java
+++ src/AbstractClass/Main.java
@@ -1,25 +1,37 @@
-//package AbstractClass;
-//
-//import java.util.Scanner;
-//
-//
-//abstract class MyBook{
-//    String title;
-//    abstract void setTitle(String s);
-//    String getTitle(){
-//        return title;
-//    }
-//}
-//
-//
-//public class Main {
-//
-//    public static void main(String[] args) {
-//        Scanner sc=new Scanner(System.in);
-//        String title=sc.nextLine();
-//        MyBook new_novel=new MyBook();
-//        new_novel.setTitle(title);
-//        System.out.println("The title is: "+new_novel.getTitle());
-//        sc.close();
-//    }
-//}
+package AbstractClass;
+
+import java.util.Scanner;
+
+
+abstract class Book{
+    String title;
+    abstract void setTitle(String s);
+    String getTitle(){
+        return title;
+    }
+}
+
+class MyBook extends Book{
+    @Override
+    void setTitle(String s) {
+        super.title = s;
+    }
+
+    @Override
+    String getTitle() {
+        return super.getTitle();
+    }
+}
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String title=sc.nextLine();
+        MyBook new_novel=new MyBook();
+        new_novel.setTitle(title);
+        System.out.println("The title is: "+new_novel.getTitle());
+        sc.close();
+    }
+}
