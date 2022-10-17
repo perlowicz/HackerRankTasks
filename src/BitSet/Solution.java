8baed138c044e3727675232a4e82befcbd385f0c
Perlowicz <bartek.perlinski1234@gmail.com>
2022-10-17 11:34:07 +0200

    new tasks

---
+++ src/BitSet/Solution.java
@@ -0,0 +1,8 @@
+package BitSet;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/Stack/Solution.java
+++ src/Stack/Solution.java
@@ -5,88 +5,32 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Stack;
 import java.util.stream.Collectors;
 
 public class Solution {
 
     public static void main(String[] args) throws IOException {
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        List<String> examples = br.lines().collect(Collectors.toList());
-//        br.close();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        List<String> examples = br.lines().toList();
+        br.close();
 
-        List<String> examples = List.of(
-                "({}[])",
-                "(({()})))",
-                "({(){}()})()({(){}()})(){()}",
-                "{}()))(()()({}}{}",
-                "}}}}",
-                "))))",
-                "{{{",
-                "(((",
-                "[]{}(){()}((())){{{}}}{()()}{{}{}}",
-                "[[]][][]",
-                "}{"
-        );
-
-//        List<String> examples = List.of("}}}}");
-
-        readloop:
         for (String example : examples) {
             char[] chars = example.toCharArray();
-            MyStack myStack = new MyStack(chars.length);
+            Stack<Character> myStack = new Stack<>();
+            boolean isOk = true;
             for (char singleChar : chars) {
-                try {
-                    if (singleChar == '{' || singleChar == '(' ||
-                        singleChar == '[')
-                        myStack.push(singleChar);
-                    else myStack.pop();
-                } catch (ArrayIndexOutOfBoundsException e){
-                    System.out.println("false");
+                if (singleChar == '{' || singleChar == '[' ||
+                    singleChar == '(')
+                    myStack.push(singleChar);
+                else if (!myStack.isEmpty())
+                    myStack.pop();
+                else {
+                    isOk = false;
+                    break;
                 }
             }
-            System.out.println(myStack.size() == 0 ? "true" : "false");
+            System.out.println(isOk && myStack.isEmpty());
         }
-    }
-}
-
-class MyStack {
-    private char[] arr;
-    public int top;
-    private int capacity;
-    MyStack(int size){
-        arr = new char[size];
-        capacity = size;
-        top = -1;
-    }
-
-    void push(char x){
-        arr[++top] = x;
-    }
-
-    char pop(){
-        peek();
-        return arr[top--];
-    }
-
-    char peek(){
-        if (!isEmpty())
-            return arr[top];
-        return '1';
-    }
-
-    public boolean isFull() {
-        return top == capacity - 1;
-    }
-
-    public boolean isEmpty() {
-        return top == -1;
-    }
-
-    public int size(){
-        return top + 1;
-    }
-
-    public String toString(){
-        return Arrays.toString(arr);
     }
 }
